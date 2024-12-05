3d93b33d4538977d2203a0ee26a43760873ea1ec
yu-k-m <77780624+youkm1@users.noreply.github.com>
2024-12-05 09:51:27 +0900

    Create leet-121.java

---
+++ 1205/leet-121.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0, minPrice= prices[0];
+        for (int price:prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price-minPrice);
+        }
+        return maxProfit;
+    }
+}
